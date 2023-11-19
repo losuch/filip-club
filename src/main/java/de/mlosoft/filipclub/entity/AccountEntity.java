602d24a04f08a7fbcfc0a6c65749ddc7304f7aaa
Lukasz Osuch <lukasz.osuch@iqvia.com>
2023-11-19 14:33:44 +0100

    update db account table

--- src/main/java/de/mlosoft/filipclub/entity/AccountEntity.java
+++ src/main/java/de/mlosoft/filipclub/entity/AccountEntity.java
@@ -10,7 +10,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "account")
 public class AccountEntity {
@@ -22,39 +26,9 @@ public class AccountEntity {
 	private String hashedPassword;
 	private String email;
 
+	private String role;
+
 	@CreationTimestamp
 	@Column(name = "created_at", updatable = false)
 	private Date createdAt;
-
-	public long getAccountId() {
-		return accountId;
-	}
-
-	public void setAccountId(long accountId) {
-		this.accountId = accountId;
-	}
-
-	public String getHashedPassword() {
-		return hashedPassword;
-	}
-
-	public void setHashedPassword(String hashedPassword) {
-		this.hashedPassword = hashedPassword;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Date getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
 }
--- src/main/java/de/mlosoft/filipclub/persistance/AccountRepositoryImpl.java
+++ src/main/java/de/mlosoft/filipclub/persistance/AccountRepositoryImpl.java
@@ -1,9 +1,5 @@
 package de.mlosoft.filipclub.persistance;
 
-import java.util.List;
-
-import de.mlosoft.filipclub.model.Account;
-
 public class AccountRepositoryImpl implements AccountRepository {
 
 }
