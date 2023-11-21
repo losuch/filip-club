0f8fdd2ac9e334b0d3136c47e7b2a228827a59a7
Lukasz Osuch <lukasz.osuch@iqvia.com>
2023-11-21 19:53:25 +0100

    adding spring doc

--- src/main/java/de/mlosoft/filipclub/controller/UtilityController.java
+++ src/main/java/de/mlosoft/filipclub/controller/UtilityController.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +28,7 @@ public class UtilityController {
     @Value("${filipclub.apiversion}")
     private String apiVersion;
 
-    @RequestMapping({ "/health" })
+    @GetMapping({ "/health" })
     @JsonSerialize
     @ResponseBody
     public HealthResponse healthCheck() {
